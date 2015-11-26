692b972bd5ad3e5afb37a7d7c7f3d6b31e57cb59
ThieryMichel <thiery@marmelab.com>
2015-11-26 11:52:06 +0100

    add limitersToQueryString Services and test it

---
+++ test/mocha/services/limitersToQueryString.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import limitersToQueryString from '../../../lib/services/limitersToQueryString';
+
+describe('limitersToQueryString', function () {
+    it('should return empty string if there is no limiter', function () {
+        assert.equal(limitersToQueryString({}), '');
+    });
+
+    it('should ignore fullText if it is false', function () {
+        assert.equal(limitersToQueryString({ fullText: false }), '');
+    });
+
+    it('should ignore unknown limiters', function () {
+        assert.equal(limitersToQueryString({ whatever: 'yop' }), '');
+    });
+
+    it ('should return FT=Y, if fullText is true', function () {
+        assert.equal(limitersToQueryString({ fullText: true }), 'FT=Y');
+    });
+
+    it ('should return DT1=${to}/${from}, if fullText is true', function () {
+        assert.equal(limitersToQueryString({ publicationDate: {
+            from: 'from',
+            to: 'to'
+        } }), 'DT1=from/to');
+    });
+
+    it ('should separate different delimiter wit "&"', function () {
+        assert.equal(limitersToQueryString({
+            fullText: true,
+            publicationDate: {
+                from: 'from',
+                to: 'to'
+            }
+        }), 'FT=Y&DT1=from/to');
+    });
+});
