51f592bf0e17a465c049c3eb348232806bfba25c
ThieryMichel <thiery@marmelab.com>
2016-01-06 17:03:36 +0100

    avoid duplicate query (when changing page)

--- test/mocha/reducers/history.js
+++ test/mocha/reducers/history.js
@@ -1,12 +1,24 @@
+import { SEARCH_SUCCESS } from '../../../lib/actions';
 import history, { getHistoryFromStorage } from '../../../lib/reducers/history';
 
-describe('reducer history', function () {
+describe.only('reducer history', function () {
     it('should return empty array as default state', function () {
         window.localStorage = {
             length: 0
         };
         assert.deepEqual(history(undefined, {}), []);
         delete window.localStorage;
+    });
+
+    describe('action SEARCH_SUCCESS', function () {
+
+        it('should add action.query to state', function () {
+            assert.deepEqual(history([{ term: 'phylloxera' }], { type: SEARCH_SUCCESS, query: { term: 'aids' }}), [{ term: 'phylloxera' }, { term: 'aids' }]);
+        });
+
+        it('should not add action.query to state if it is already present in state', function () {
+            assert.deepEqual(history([{ term: 'phylloxera' }, { term: 'aids' }], { type: SEARCH_SUCCESS, query: { term: 'aids' }}), [{ term: 'phylloxera' }, { term: 'aids' }]);
+        });
     });
 
     describe('getHistoryFromStorage', function () {
