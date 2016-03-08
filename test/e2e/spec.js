40ee5fa526a2c0f8d2d286a267bb5b328890d864
ThieryMichel <thiery@marmelab.com>
2016-03-08 16:38:32 +0100

    bootstrap nightwatch and selenium

---
+++ test/e2e/spec.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Nightwatch.js Test' : function (browser) {
+        browser
+        .url(browser.launch_url)
+        .waitForElementVisible('body', 1000)
+        .setValue('.username', 'nightwatch')
+        .setValue('.password', 'password')
+        .click('button')
+        .waitForElementVisible('.error', 100)
+        .end();
+    }
+};
