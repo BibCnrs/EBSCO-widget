922d3abdc6541e7e091c26d95aa76c58a5a0aeec
ThieryMichel <thiery@marmelab.com>
2016-03-14 10:26:54 +0100

    test edit refresh and delete in history

--- test/e2e/test/history.js
+++ test/e2e/test/history.js
@@ -17,4 +17,37 @@ describe('history', function() {
 
         client.start(done);
     });
+
+    it('should reload search in history', function (done) {
+        browser
+        .waitForElementVisible('.history', 100)
+        .click('tr:nth-child(2) .edit')
+        .assert.value('.article-search-input-list input', 'aids')
+        .assert.value('.fullText input', 'on')
+        .assert.containsText('.language .Select-value-label', 'English')
+        .assert.elementCount('.record', 0);
+
+        client.start(done);
+    });
+
+    it('should relaunch search in history', function (done) {
+        browser
+        .waitForElementVisible('.history', 100)
+        .click('tr:nth-child(2) .refresh')
+        .assert.value('.article-search-input-list input', 'aids')
+        .assert.value('.fullText input', 'on')
+        .assert.containsText('.language .Select-value-label', 'English')
+        .assert.elementCount('.record', 20);
+
+        client.start(done);
+    });
+
+    it('should delete search in history', function (done) {
+        browser
+        .waitForElementVisible('.history', 100)
+        .click('tr:nth-child(2) .delete')
+        .assert.containsText('tr:nth-child(2)', 'Tout: phylloxera vie Texte intégral 1193 résultats');
+
+        client.start(done);
+    });
 });
