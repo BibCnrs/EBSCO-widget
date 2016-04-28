3ab98d5f81cbe8b9c95b3dd67d9690901d4f4571
ThieryMichel <thiery@marmelab.com>
2016-04-28 18:01:53 +0200

    refactor e2e logion test

--- mock/server.js
+++ mock/server.js
@@ -1,24 +1,13 @@
 import koa from 'koa';
 import route from 'koa-route';
-import cobody from 'co-body';
 import cors from 'koa-cors';
 
 const app = koa();
 
 app.use(cors({origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], headers: ['Content-Type', 'Authorization']}));
 
-app.use(route.post('/login', function* () {
-    const { username, password} = yield cobody(this);
-    if (username !== 'test' || password !== 'secret') {
-        this.status = 401;
-
-        return;
-    }
-    this.body = {
-        token: 'token',
-        domains: ['vie', 'shs']
-    };
-
+app.use(route.get('/login', function* () {
+    this.redirect(`http://app?shib=cookie%3Dcookie&token=token&domains=vie&domains=shs&username=tester`);
 }));
 
 app.use(route.get('/:domainName/article/search', function* articleSearch(domainName) {
