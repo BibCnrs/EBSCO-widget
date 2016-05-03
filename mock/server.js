b38ce31d3fc8912001153b5e7093fc2bb9a54d0c
ThieryMichel <thiery@marmelab.com>
2016-05-03 18:19:38 +0200

    fix e2e login test

--- mock/server.js
+++ mock/server.js
@@ -1,12 +1,27 @@
 import koa from 'koa';
 import route from 'koa-route';
+import cobody from 'co-body';
 import cors from 'koa-cors';
 
 const app = koa();
 
 app.use(cors({origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'], headers: ['Content-Type', 'Authorization']}));
 
-app.use(route.get('/login', function* () {
+app.use(route.post('/login', function* () {
+    const { username, password} = yield cobody(this);
+    if (username !== 'test' || password !== 'secret') {
+        this.status = 401;
+
+        return;
+    }
+    this.body = {
+        token: 'token',
+        domains: ['vie', 'shs']
+    };
+
+}));
+
+app.use(route.get('/login_renater', function* () {
     this.redirect(`http://app?shib=cookie%3Dcookie&token=token&domains=vie&domains=shs&username=tester`);
 }));
 
