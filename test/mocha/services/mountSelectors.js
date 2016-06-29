a894938b4bc3d0522d2f2f57f630cff2d5cc8c2c
ThieryMichel <thiery@marmelab.com>
2016-06-29 17:03:31 +0200

    add mountSelectors service

---
+++ test/mocha/services/mountSelectors.js
@@ -0,0 +1,34 @@
+import mountSelectors from '../../../lib/services/mountSelectors';
+
+describe('mountSelectors', function () {
+    it('should return mountedSelectors', function () {
+        let get1Call, get2Call;
+
+        const selectors = {
+            get1: (...args) => get1Call = args,
+            get2: (...args) => get2Call = args
+        };
+
+        const mountedSelectors = mountSelectors('subStateKey', selectors);
+
+        assert.deepEqual(Object.keys(mountedSelectors), ['get1', 'get2']);
+
+        mountedSelectors.get1({ subStateKey: 'subState' }, 'additional', 'arguments');
+
+        assert.deepEqual(get1Call, ['subState', 'additional', 'arguments']);
+        assert.isUndefined(get2Call);
+    });
+
+    it('should discard selectors that are not function', function () {
+
+        const selectors = {
+            get1: () => null,
+            get2: () => null,
+            data: 'value'
+        };
+
+        const mountedSelectors = mountSelectors('subStateKey', selectors);
+
+        assert.deepEqual(Object.keys(mountedSelectors), ['get1', 'get2']);
+    });
+});
