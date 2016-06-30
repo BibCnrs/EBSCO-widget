534a26d6c74205627fc0d4f676da4cc46c72d9d6
ThieryMichel <thiery@marmelab.com>
2016-06-30 11:08:37 +0200

    have expoortNotice saga check if user is logged

--- test/mocha/sagas/exportNotice.js
+++ test/mocha/sagas/exportNotice.js
@@ -1,40 +1,69 @@
 import { put, select } from 'redux-saga/effects';
 
 import { exportNotice } from '../../../lib/sagas/exportNotice';
-import { fromNotice } from '../../../lib/reducers';
+import { fromNotice, fromLogin } from '../../../lib/reducers';
 import {
     EXPORT_NOTICE_PENDING,
     EXPORT_NOTICE_SUCCESS
 } from '../../../lib/actions';
 
-describe('sagas export notice', function () {
+describe.only('sagas export notice', function () {
     let iterator;
-
-    before(function () {
+    beforeEach(function () {
         iterator = exportNotice({ category: 'category', ids: [1, 2, 3] });
     });
 
-    it('should trigger EXPORT_NOTICE_PENDING action', function () {
+    it('should select isUserLogged', function () {
         const next = iterator.next();
+        assert.deepEqual(next.value, select(fromLogin.isUserLogged));
+    });
+
+    it('should trigger EXPORT_NOTICE_PENDING action if user isLogged', function () {
+        iterator.next();
+        const next = iterator.next(true);
         assert.deepEqual(next.value, put({
             type: EXPORT_NOTICE_PENDING,
             category: 'category'
         }));
     });
 
+    it('should end if user is not logged', function () {
+        iterator.next();
+        const next = iterator.next(false);
+        assert.isTrue(next.done);
+    });
+
     it('should select missingNoticeIds', function () {
+        iterator.next();
+        iterator.next(true);
         const next = iterator.next();
         assert.deepEqual(next.value, select(fromNotice.getMissingNoticeIds, 'category', [1, 2, 3]));
     });
 
     it('should select notices by ids if no missing ids', function () {
         const missingIds = [];
+        iterator.next();
+        iterator.next(true);
+        iterator.next();
         const next = iterator.next(missingIds);
         assert.deepEqual(next.value, select(fromNotice.getNoticesByIds, 'category', [1, 2, 3]));
     });
 
+    it('should end if missing ids', function () {
+        const missingIds = [1];
+        iterator.next();
+        iterator.next(true);
+        iterator.next();
+        const next = iterator.next(missingIds);
+        assert.isTrue(next.done);
+    });
+
     it('should trigger EXPORT_NOTICE_SUCCESS with returned notices', function () {
         const notices = ['notice1', 'notice2', 'notice3'];
+        iterator.next();
+        iterator.next(true);
+        iterator.next();
+        iterator.next([]);
         const next = iterator.next(notices);
         assert.deepEqual(next.value, put({
             type: EXPORT_NOTICE_SUCCESS,
