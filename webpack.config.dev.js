830199ab0ea7ad6964d5629284de55efe5e0314a
ThieryMichel <thiery@marmelab.com>
2016-08-05 16:21:09 +0200

    add background image

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -34,6 +34,7 @@ module.exports = {
             },
             sassLoader,
             { test: /\.css$/, loader: 'style-loader!css-loader?importLoaders=1' },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' },
             {
                 test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,
                 loader: 'url-loader?limit=8192'
