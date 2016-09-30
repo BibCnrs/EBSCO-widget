f0377d9a9892ad2e3a6cb52a3caacd943c9acb45
ThieryMichel <thiery@marmelab.com>
2016-09-30 17:11:55 +0200

    add test

--- test/mocha/sagas/fetch.js
+++ test/mocha/sagas/fetch.js
@@ -1,7 +1,9 @@
-import { race, call, take } from 'redux-saga/effects';
+import { race, call, take, put } from 'redux-saga/effects';
 
 import fetch from '../../../lib/sagas/fetch';
 import fetchServices from '../../../lib/services/fetch';
+import actions,  {DISCONNECTED} from '../../../lib/actions';
+
 
 describe('sagas fetch', function () {
     let iterator;
@@ -14,7 +16,7 @@ describe('sagas fetch', function () {
         const next = iterator.next();
         assert.deepEqual(next.value, race({
             fetchResult: call(fetchServices, request),
-            cancel: take('STOP')
+            cancel: take(['STOP', DISCONNECTED])
         }));
     });
 
@@ -31,6 +33,18 @@ describe('sagas fetch', function () {
         const next = iterator.throw('error');
 
         assert.deepEqual(next.value, { error: 'error' });
+        assert.isTrue(next.done);
+    });
+
+    it('should put logout and disconnected before returning cancel if fetchresult.error.code is 401', function () {
+        iterator.next();
+        let next = iterator.next({ fetchResult: { error: { code: 401 } } });
+
+        assert.deepEqual(next.value, put(actions.logout()));
+        next = iterator.next();
+        assert.deepEqual(next.value, put(actions.disconnected()));
+        next = iterator.next();
+        assert.deepEqual(next.value, { cancel: true });
         assert.isTrue(next.done);
     });
 
