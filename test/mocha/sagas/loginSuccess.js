537b0e797d3411b57f21bb1cf3580296391fa59d
ThieryMichel <thiery@marmelab.com>
2017-01-05 13:53:10 +0100

    add test

--- test/mocha/sagas/loginSuccess.js
+++ test/mocha/sagas/loginSuccess.js
@@ -3,6 +3,7 @@ import { put, select, call } from 'redux-saga/effects';
 import { loginSuccess } from '../../../lib/sagas/loginSuccess';
 import * as fromState from '../../../lib/selectors';
 import { sessionStorage } from '../../../lib/services/storage';
+import actions from '../../../lib/actions';
 
 describe('sagas loginSuccess', function () {
     let iterator;
@@ -17,9 +18,7 @@ describe('sagas loginSuccess', function () {
     });
 
     it('should call sessionStorage.setItem three times', function () {
-
         let next = iterator.next();
-
         assert.deepEqual(next.value, call(sessionStorage.setItem, 'EBSCO_WIDGET_username', 'john'));
         next = iterator.next();
         assert.deepEqual(next.value, call(sessionStorage.setItem, 'EBSCO_WIDGET_availableDomains', ['INSB', 'INSHS']));
@@ -27,12 +26,47 @@ describe('sagas loginSuccess', function () {
         assert.deepEqual(next.value, call(sessionStorage.setItem, 'EBSCO_WIDGET_domain', 'INSB'));
     });
 
-    it('should select pausedAction', function () {
+    it('should put noDomainError and end if no domain', () => {
+        iterator = loginSuccess({
+            response: {
+                username: 'john',
+                domains: []
+            }
+        });
         iterator.next();
         iterator.next();
         iterator.next();
 
         const next = iterator.next();
+        assert.deepEqual(next.value, put(actions.noDomainError()));
+    });
+
+    it('should select hasPublicationSearchResult', () => {
+        iterator.next();
+        iterator.next();
+        iterator.next();
+
+        const next = iterator.next();
+        assert.deepEqual(next.value, select(fromState.hasPublicationSearchResult));
+    });
+
+    it('should put changeDomain if hasPublicationSearchResult is false', () => {
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+
+        const next = iterator.next(false);
+        assert.deepEqual(next.value, put(actions.changeDomain('publication', action.response.domains[0])));
+    });
+
+    it('should select pausedAction if hasPublicationSearchResult is true', function () {
+        iterator.next();
+        iterator.next();
+        iterator.next();
+        iterator.next();
+
+        const next = iterator.next(true);
         assert.deepEqual(next.value, select(fromState.getPausedAction));
     });
 
@@ -40,8 +74,9 @@ describe('sagas loginSuccess', function () {
         iterator.next();
         iterator.next();
         iterator.next();
-
         iterator.next();
+
+        iterator.next(true);
         const next = iterator.next(undefined);
         assert.isTrue(next.done);
     });
@@ -50,8 +85,9 @@ describe('sagas loginSuccess', function () {
         iterator.next();
         iterator.next();
         iterator.next();
-
         iterator.next();
+
+        iterator.next(true);
         const next = iterator.next({ type: 'PAUSED_ACTION'});
         assert.deepEqual(next.value, put({ type: 'PAUSED_ACTION' }));
     });
