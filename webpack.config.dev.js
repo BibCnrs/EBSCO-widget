a6686703a19dd584c3415f87f0bba4df5caeb595
ThieryMichel <thiery@marmelab.com>
2018-02-07 13:59:03 +0100

    upgrade react to 16 applying code mod and deactivating broken css transition for now

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -71,7 +71,6 @@ module.exports = {
     externals: {
         'react': 'React',
         'react-dom': 'ReactDOM',
-        'react-addons-css-transition-group': 'React.addons.CSSTransitionGroup',
-        'babel-polyfill': '_babelPolyfill'
+        // 'babel-polyfill': '_babelPolyfill'
     }
 };
