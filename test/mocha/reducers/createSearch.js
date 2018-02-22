f62f9c04fc9a52ce602c4c28cd325b17f760c7df
ThieryMichel <thiery@marmelab.com>
2018-02-22 10:32:15 +0100

    Revert "Merge pull request #171 from BibCnrs/a2z_sort_title"
    This reverts commit e3dd98283a1bea751298ff99b8d55ee91ce522e4, reversing
    changes made to 84babaa6ee7a6524706c72d6c655f599cf9b6934.

--- test/mocha/reducers/createSearch.js
+++ test/mocha/reducers/createSearch.js
@@ -7,8 +7,7 @@ import {
     SEARCH_SUCCESS,
     SEARCH_ERROR,
     CHANGE_SORT,
-    CHANGE_RESULTS_PER_PAGE,
-    A2Z_SEARCH,
+    CHANGE_RESULTS_PER_PAGE
 } from '../../../lib/actions';
 
 describe('reducers createSearch', function() {
@@ -114,22 +113,6 @@ describe('reducers createSearch', function() {
                     status: 'DONE',
                     term: 'aids',
                     dateRange: 'date range',
-                });
-            });
-        });
-
-        describe('A2Z_SEARCH', () => {
-            it('should set sort to title', () => {
-                const searchState = categorySearch(
-                    { foo: 'bar' },
-                    {
-                        type: A2Z_SEARCH,
-                        category: 'category',
-                    },
-                );
-                assert.deepEqual(searchState, {
-                    foo: 'bar',
-                    sort: 'title',
                 });
             });
         });
