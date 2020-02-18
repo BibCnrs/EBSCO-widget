909c950e0dd9845d1b26e6feecd83adef98c6536
Nacim Goura <nacim.goura.dev@gmail.com>
2020-02-18 18:00:47 +0100

    modify revue page

--- test/e2e/test/authentication.js
+++ test/e2e/test/authentication.js
@@ -6,56 +6,6 @@ describe('authentication', function() {
         client.start(done);
     });
 
-    it('should display login modal when trying to retrieve clicked publication title', function(done) {
-        browser
-            .waitForElementVisible('.navbar.navbar-default', 1000)
-            .assert.containsText(
-                '.navbar.navbar-default .active',
-                'Une revue, un ouvrage',
-            )
-            .waitForElementVisible('.record', 1000)
-            .click('.record_list a.fetch-link')
-            .pause(300)
-            .waitForElementVisible('.authentication .modal-dialog', 1000)
-            .assert.containsText(
-                '.janus',
-                "Via le gestionnaire d'identité janus",
-            )
-            .assert.containsText(
-                '.inist-button',
-                "Via votre ancien code d'accès portail",
-            )
-            .click('.inist-button')
-            .waitForElementVisible('.username', 1000)
-            .setValue('.username', 'test')
-            .setValue('.password', 'secret')
-            .click('button.api')
-            .pause(2000)
-            .waitForElementVisible('.notice', 1000)
-            .assert.containsText('.notice dl span:nth-child(1) dt', 'ISSN')
-            .assert.containsText(
-                '.notice dl span:nth-child(2) dt',
-                'Info éditeur',
-            )
-            .assert.containsText(
-                '.notice dl span:nth-child(3) dt',
-                'Type de publication',
-            )
-            .assert.containsText('.notice dl span:nth-child(4) dt', 'Mot clé')
-            .assert.containsText(
-                '.notice dl span:nth-child(5) dt',
-                'Description',
-            )
-            .assert.containsText('.notice dl span:nth-child(6) dt', 'URL')
-            .assert.containsText('.notice dl span:nth-child(7) dt', 'Fréquence')
-            .assert.containsText(
-                '.notice dl span:nth-child(8) dt',
-                'Relu par un comité de lecture',
-            );
-
-        client.start(done);
-    });
-
     it('should display login modal when trying to search article', function(done) {
         browser
             .waitForElementVisible('.navbar.navbar-default', 1000)
