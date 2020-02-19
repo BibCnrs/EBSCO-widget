5ad0faf963747df3f7898315daac57282f0955b4
nacimgoura <nacim.goura.dev@gmail.com>
2020-02-19 15:13:29 +0100

    2.8.1

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,15 +7,16 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var version = require('./package.json').version;
 
 var sassOptions = [
-    'includePaths[]='+ path.resolve(__dirname, './lib/sass/'),
-    'includePaths[]='+ path.resolve(__dirname, './node_modules/compass-mixins/lib'),
-    'outputStyle=expanded'
+    'includePaths[]=' + path.resolve(__dirname, './lib/sass/'),
+    'includePaths[]=' +
+        path.resolve(__dirname, './node_modules/compass-mixins/lib'),
+    'outputStyle=expanded',
 ].join('&');
 
 var sassLoader = {
     test: /\.scss/g,
     exclude: /node_modules/,
-    loaders: ['style', 'css', 'sass?' + sassOptions]
+    loaders: ['style', 'css', 'sass?' + sassOptions],
 };
 
 module.exports = {
@@ -27,50 +28,53 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     cacheDirectory: true,
-                    presets: ['react', 'es2015', 'stage-2']
+                    presets: ['react', 'es2015', 'stage-2'],
                 },
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
             sassLoader,
-            { test: /\.css$/, loader: 'style-loader!css-loader?importLoaders=1' },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?importLoaders=1',
+            },
             {
                 test: /\.(otf|eot|svg|ttf|woff|woff2|png|jpg)(\?.+)?$/,
-                loader: 'url-loader?limit=8192'
-            }
-        ]
+                loader: 'url-loader?limit=8192',
+            },
+        ],
     },
     entry: {
         app: [
             './lib/index.js',
             './lib/sass/style.scss',
-            'webpack-dev-server/client?http://localhost:3001'
-        ]
+            'webpack-dev-server/client?http://localhost:3001',
+        ],
     },
     output: {
         // Make sure to use [name] or [id] in output.filename
         //  when using multiple entry points
         filename: 'main.js',
         chunkFilename: '[id].bundle.js',
-        publicPath: 'http://localhost:3001/'
+        publicPath: 'http://localhost:3001/',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './lib/index.html',
-            inject: 'body'
+            inject: 'body',
         }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             __DEVELOPMENT__: true,
             __VERSION__: JSON.stringify(version),
-            __SERVER_URL__: JSON.stringify(process.env.server_url)
+            __SERVER_URL__: JSON.stringify(process.env.server_url),
         }),
         new ExtractTextPlugin('build/app.css', {
-            allChunks: true
-        })
+            allChunks: true,
+        }),
     ],
     externals: {
-        'react': 'React',
+        react: 'React',
         'react-dom': 'ReactDOM',
         'babel-polyfill': '_babelPolyfill',
-    }
+    },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,15 +7,16 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var version = require('./package.json').version;
 
 var sassOptions = [
-    'includePaths[]='+ path.resolve(__dirname, './lib/sass/'),
-    'includePaths[]='+ path.resolve(__dirname, './node_modules/compass-mixins/lib'),
-    'outputStyle=expanded'
+    'includePaths[]=' + path.resolve(__dirname, './lib/sass/'),
+    'includePaths[]=' +
+        path.resolve(__dirname, './node_modules/compass-mixins/lib'),
+    'outputStyle=expanded',
 ].join('&');
 
 var sassLoader = {
     test: /\.scss/g,
-    exclude: /node_modules/ ,
-    loader: ExtractTextPlugin.extract('css!sass?' + sassOptions)
+    exclude: /node_modules/,
+    loader: ExtractTextPlugin.extract('css!sass?' + sassOptions),
 };
 
 module.exports = {
@@ -27,47 +28,51 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     cacheDirectory: true,
-                    presets: ['react', 'es2015', 'stage-2']
+                    presets: ['react', 'es2015', 'stage-2'],
                 },
-                exclude: /node_modules/
+                exclude: /node_modules/,
             },
-            { test: /\.(woff2?|svg|ttf|eot|jpe?g|gif|ico)?$/, loader: 'file?name=[path][hash].[ext]&context=./src', exclude: /node_modules/ },
+            {
+                test: /\.(woff2?|svg|ttf|eot|jpe?g|gif|ico)?$/,
+                loader: 'file?name=[path][hash].[ext]&context=./src',
+                exclude: /node_modules/,
+            },
             sassLoader,
-            { test: /\.css$/, loader: 'style-loader!css-loader?importLoaders=1' },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?importLoaders=1',
+            },
             {
                 test: /\.(otf|eot|svg|ttf|woff|woff2|png|jpg)(\?.+)?$/,
-                loader: 'url-loader'
-            }
-        ]
+                loader: 'url-loader',
+            },
+        ],
     },
     entry: {
-        app: [
-            './lib/index.js',
-            './lib/sass/style.scss'
-        ]
+        app: ['./lib/index.js', './lib/sass/style.scss'],
     },
     output: {
         // Make sure to use [name] or [id] in output.filename
         //  when using multiple entry points
         path: './build',
         filename: '[name].js',
-        chunkFilename: '[id].bundle.js'
+        chunkFilename: '[id].bundle.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './lib/index.html',
-            inject: 'body'
+            inject: 'body',
         }),
         new ExtractTextPlugin('[name].css'),
         new webpack.DefinePlugin({
             __DEVELOPMENT__: false,
             __VERSION__: JSON.stringify(version),
-            __SERVER_URL__: null
-        })
+            __SERVER_URL__: null,
+        }),
     ],
     externals: {
-        'react': 'React',
+        react: 'React',
         'react-dom': 'ReactDOM',
         'babel-polyfill': '_babelPolyfill',
-    }
+    },
 };
