e579ca408d5a5c7853cf06f8f615b67495930d69
join <florence.join@inist.fr>
2020-09-11 11:02:22 +0200

    fix search revue international journal of american linguistics  INSHS : no dropdown list

--- mock/launcher.js
+++ mock/launcher.js
@@ -6,6 +6,8 @@ var app = require('./server');
 if (!module.parent) {
     app.listen(80);
 
+    // eslint-disable-next-line no-console
     console.log('Mock server listening on port 80');
+    // eslint-disable-next-line no-console
     console.log('Press CTRL+C to stop server');
 }
