27a43a55c1c0d44171a5d409e9be875d4825ff75
join <florence.join@inist.fr>
2022-02-11 15:53:15 +0100

    upgrade react 17

--- test/mocha/setup.js
+++ test/mocha/setup.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import { assert } from 'chai';
 import Enzyme, * as enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 var React = require('react');
 
