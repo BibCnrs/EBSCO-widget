c1a91576febf72bc7ec22698822fad948c478f91
Anthony Rimet <anthony.rimet03@gmail.com>
2022-11-04 10:59:32 +0100

    Fix: Download license

--- test/mocha/components/Database.js
+++ test/mocha/components/Database.js
@@ -1,6 +1,6 @@
 import { Database, DatabaseLetter } from '../../../lib/components/Database';
 
-describe.only('Database', function() {
+describe('Database', function() {
     it('should render one DatabaseLetter for eack key in databases', function() {
         const props = {
             language: 'fr',
